09c3d176c441f17981f3282689c25bf9525ea83c
ohhyunsok <terecal@daum.net>
2025-02-16 00:08:53 +0900

    update for 사이즈 조절

--- src/widgets/sidebar/index.tsx
+++ src/widgets/sidebar/index.tsx
@@ -3,29 +3,22 @@
 import React, { useCallback, useState, useEffect } from "react";
 import { Resizable } from "re-resizable";
 import { useSidebarStore } from "@/store/useSidebarStore";
-import { debounce } from 'lodash';
 import { SidebarHeader } from './ui/SidebarHeader';
 import { TabContent } from './ui/TabContent';
 import { SidebarToggleButton } from "./ui/SidebarToggleButton";
-import { useSidebarTabStore } from "./model/store";
 import { SidebarTabs } from "./ui/SidebarTabs";
 
 export default function Sidebar() {
   const { isOpen, width, setWidth } = useSidebarStore();
   const toggleSidebar = useSidebarStore((state) => state.toggleSidebar);
   const [isResizing, setIsResizing] = useState(false);
+  const [currentWidth, setCurrentWidth] = useState(width);
   const [isInitialized, setIsInitialized] = useState(false);
 
   useEffect(() => {
     setIsInitialized(true);
-  }, []);
-
-  const debouncedSetWidth = useCallback(
-    debounce((width: number) => {
-      setWidth(width);
-    }, 100),
-    []
-  );
+    setCurrentWidth(width);
+  }, [width]);
 
   const handleResizeStart = useCallback(() => {
     setIsResizing(true);
@@ -33,14 +26,14 @@ export default function Sidebar() {
   }, []);
 
   const handleResize = useCallback((_: any, __: any, ref: HTMLElement) => {
-    const newWidth = ref.clientWidth;
-    debouncedSetWidth(newWidth);
-  }, [debouncedSetWidth]);
+    setCurrentWidth(ref.clientWidth);
+  }, []);
 
   const handleResizeStop = useCallback(() => {
     setIsResizing(false);
     document.body.style.cursor = 'default';
-  }, []);
+    setWidth(currentWidth);
+  }, [currentWidth, setWidth]);
 
   if (!isInitialized) {
     return <div className="h-full opacity-0" />;
@@ -48,10 +41,10 @@ export default function Sidebar() {
 
   return (
     <div className="h-full flex flex-col">
-      <div className="flex-1 relative">
+      <div className="flex-1 relative select-none">
         <Resizable
           size={{
-            width: isOpen ? width : 16,
+            width: isOpen ? currentWidth : 16,
             height: '100%',
           }}
           minWidth={16}
@@ -74,15 +67,13 @@ export default function Sidebar() {
               position: 'absolute',
               cursor: 'ew-resize',
               zIndex: 50,
+              touchAction: 'none',
             }
           }}
           onResizeStart={handleResizeStart}
           onResize={handleResize}
           onResizeStop={handleResizeStop}
           className="bg-white h-full relative"
-          style={{
-            transition: isResizing ? 'none' : 'width 300ms ease-in-out',
-          }}
         >
           {/* 토글 버튼 */}
           <div className="absolute -right-2 top-3 z-40">
@@ -93,7 +84,6 @@ export default function Sidebar() {
             className={`
               h-full flex flex-col
               ${!isOpen && 'w-0 overflow-hidden'}
-              ${isResizing ? '' : 'transition-all duration-300 ease-in-out'}
             `}
           >
             <SidebarHeader />
@@ -102,16 +92,14 @@ export default function Sidebar() {
             </div>
           </div>
 
-          {/* 리사이즈 핸들 시각적 표시 */}
-          <div 
-            className={`
-              absolute top-0 -right-px w-px h-full bg-gray-200 z-30 pointer-events-none
-              after:absolute after:inset-0 after:border-r after:border-dashed after:border-gray-300
-            `}
-            style={{
-              transition: isResizing ? 'none' : 'all 300ms ease-in-out',
-            }}
-          />
+          {!isResizing && (
+            <div 
+              className="absolute top-0 -right-px w-px h-full bg-gray-200 z-30 pointer-events-none"
+              style={{
+                borderRight: '1px dashed #d1d5db',
+              }}
+            />
+          )}
         </Resizable>
       </div>
 
@@ -119,10 +107,11 @@ export default function Sidebar() {
       <div
         className={`
           bg-white flex-shrink-0
-          ${isResizing ? '' : 'transition-all duration-300 ease-in-out'}
           ${!isOpen ? 'w-16' : ''}
         `}
-        style={{ width: isOpen ? width : 16 }}
+        style={{ 
+          width: isOpen ? currentWidth : 16,
+        }}
       >
         <SidebarTabs />
       </div>
